d005d1a40302c2252461504b38b51fe46876671f
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2021-07-17 19:38:07 +0100

    UI update

--- app/src/main/java/com/example/newdo/database/ArticleDatabase.kt
+++ app/src/main/java/com/example/newdo/database/ArticleDatabase.kt
@@ -11,7 +11,7 @@ import com.example.newdo.database.model.Article
     entities = [Article::class],
     version = 1
 )
-@TypeConverters(Converters::class )
+@TypeConverters(Converters::class)
 
 abstract class ArticleDatabase : RoomDatabase() {
 
