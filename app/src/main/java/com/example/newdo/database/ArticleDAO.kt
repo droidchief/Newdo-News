64098332eeabd42c06436551953dfa5de6377fd5
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2021-07-17 18:43:06 +0100

    set up DAO for articles

---
+++ app/src/main/java/com/example/newdo/database/ArticleDAO.kt
@@ -0,0 +1,19 @@
+package com.example.newdo.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.newdo.model.Article
+
+@Dao
+interface ArticleDAO  {
+
+     @Insert(onConflict = OnConflictStrategy.REPLACE)
+     suspend fun uosert(article: Article) : Long
+
+     @Query("SELECT * FROM articles")
+     fun getAllSavedArticles(): LiveData<List<Article>>
+
+     @Delete
+     suspend fun deleteArticle(article: Article)
+
+}
