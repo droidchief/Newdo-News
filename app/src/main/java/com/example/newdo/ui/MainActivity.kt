30862ea371d58d21d93e5f85cfc74f8b698eea3a
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2021-07-23 20:49:11 +0100

    feat: popup menu

--- app/src/main/java/com/example/newdo/ui/MainActivity.kt
+++ app/src/main/java/com/example/newdo/ui/MainActivity.kt
@@ -49,7 +49,6 @@ class MainActivity : AppCompatActivity() {
             return@setOnItemReselectedListener
         }
 
-
     }
 
     private fun disableDarkMode() {
--- app/src/main/java/com/example/newdo/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/newdo/ui/fragments/ArticleFragment.kt
@@ -1,18 +1,19 @@
 package com.example.newdo.ui.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.webkit.WebViewClient
+import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.newdo.R
+import com.example.newdo.database.model.Article
 import com.example.newdo.databinding.FragmentArticleBinding
 import com.example.newdo.ui.MainActivity
 import com.example.newdo.ui.viewmodels.NewsViewModel
@@ -25,11 +26,17 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
     lateinit var viewModel: NewsViewModel
     val args: ArticleFragmentArgs by navArgs()
 
+    private lateinit var article: Article
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentArticleBinding.bind(view)
 
         viewModel = (activity as MainActivity).viewModel
+
+        //get current article passed from previous page
+        article = args.article
+
 
         loadWebView(view)
 
@@ -41,12 +48,44 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
             }, 4000)
         }
 
+        //popup menu
+        binding.menuBtn.setOnClickListener {
+            val popupMenu = PopupMenu(requireContext(), it)
+            popupMenu.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.refresh -> {
+                        Toast.makeText(requireContext(), "Refresh", Toast.LENGTH_SHORT)
+                            .show()
+                        true
+                    }
+                    R.id.openInBrowser -> {
+                        Toast.makeText(requireContext(), "Open in Browser", Toast.LENGTH_SHORT)
+                            .show()
+                        true
+                    }
+                    R.id.copyLink -> {
+                        Toast.makeText(requireContext(), "Copy Link", Toast.LENGTH_SHORT)
+                            .show()
+                        true
+                    }
+                    R.id.shareArticle -> {
+                        Toast.makeText(requireContext(), "Share", Toast.LENGTH_SHORT)
+                            .show()
+                        true
+                    }
+
+                    else -> false
+                }
+
+            }
+
+            popupMenu.inflate(R.menu.article_menu)
+            popupMenu.show()
+        }
+
     }
 
     private fun loadWebView(view: View) {
-        //get current article passed from previous page
-        val article = args.article
-
         //pass news detail
         Glide.with(this).load(article.urlToImage).into(binding.articleImage)
         binding.author.text = "Author - ${article.author}"
